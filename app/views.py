40ef3146194aec8fa4f5254511918f08266530d9
nexusameer <ameerk10fw@gmail.com>
2025-03-01 10:45:47 +0500

    Revert "push"
    This reverts commit 8c7d4875869f3d93705d672d173809fe84bbb6f0.

--- app/views.py
+++ app/views.py
@@ -8,7 +8,7 @@ from django.shortcuts import get_object_or_404
 # Create your views here.
 def index(request):
     person = Person.load()
-    background = Background.objects.all().order_by('-id')
+    background = Background.objects.all()
     skills = Skills.objects.all()
     project = Projects.objects.all()
     languages = Languages.objects.all()
