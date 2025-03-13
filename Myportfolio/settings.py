e3224458c0ab1b579f4e2b70e05346be74117584
nexusameer <ameerk10fw@gmail.com>
2025-03-13 14:38:02 +0500

    index page and setting for kubernetes

--- Myportfolio/settings.py
+++ Myportfolio/settings.py
@@ -130,6 +130,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+FORCE_SCRIPT_NAME = os.getenv('FORCE_SCRIPT_NAME', '')
+
+
 STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
